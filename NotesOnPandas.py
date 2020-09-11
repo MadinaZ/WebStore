3b08c3d4fb19628db209641645218fa6a99ba88f
Madina <42979064+MadinaZ@users.noreply.github.com>
2020-09-12 03:31:36 +0600

    Update NotesOnPandas.py

--- NotesOnPandas.py
+++ NotesOnPandas.py
@@ -8,7 +8,6 @@ y = home_data['SalePrice']
 melbourne_data = melbourne_data.dropna(axis=0)
 
 #To calculate MAE, we first need a model
-# Data Loading Code Hidden Here
 import pandas as pd
 
 # Load data
@@ -33,3 +32,18 @@ from sklearn.metrics import mean_absolute_error
 
 predicted_home_prices = melbourne_model.predict(X)
 mean_absolute_error(y, predicted_home_prices)
+
+#Dealing w Missing Values: Imputation
+from sklearn.impute import SimpleImputer
+
+# Imputation
+my_imputer = SimpleImputer()
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+# Imputation removed column names; put them back
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+print("MAE from Approach 2 (Imputation):")
+print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
